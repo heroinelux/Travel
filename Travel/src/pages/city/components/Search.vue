e211f53ea038492827a9bdbc95744f6eb2e847fb
heroinelux <18810712191@163.com>
2018-06-23 20:42:22 +0800

    add city search logic

--- Travel/src/pages/city/components/Search.vue
+++ Travel/src/pages/city/components/Search.vue
@@ -1,12 +1,62 @@
 <template>
-	<div class="search">
-		<input type="text" class="search-input" placeholder="输入城市名或拼音">
+	<div>
+		<div class="search">
+			<input type="text" class="search-input" placeholder="输入城市名或拼音" v-model="keyword">
+		</div>
+		<div class="search-content" v-show="keyword" ref="search">
+			<ul>
+				<li class="search-item" v-for="item of list" :key="item.id">{{item.name}}
+				</li>
+				<li class="search-item" v-show="hasNoData">没有找到匹配数据</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
 <script>
+	import Bscroll from "better-scroll"
 	export default{
-		name:"CitySerach"
+		name:"CitySerach",
+		props:{
+			cities:Object
+		},
+		data(){
+			return{
+				keyword:"",
+				timer:null,
+				list:[]
+			}
+		},
+		computed:{
+			hasNoData(){
+				return !this.list.length;
+			}
+		},
+		watch:{
+			keyword(){
+				if(this.timer){
+					clearTimeout(this.timer);
+				}
+				if(!this.keyword){
+					this.list=[];
+					return;
+				}
+				this.timer=setTimeout(()=>{
+					const result=[];
+					for(let i in this.cities){
+						this.cities[i].forEach((value)=>{
+							if(value.spell.search(this.keyword)!=-1||value.name.search(this.keyword)!=-1){
+								result.push(value);
+							}
+						});
+					}
+					this.list=result;
+				},100);
+			}
+		},
+		mounted(){
+			this.scroll=new Bscroll(this.$refs.search);
+		}
 	}
 </script>
 
@@ -24,5 +74,19 @@
 			border-radius:.06rem
 			padding:0 .1rem
 			box-sizing:border-box
+			color:#666
+	.search-content	
+		background:#eee
+		position:absolute
+		top:1.58rem
+		right:0
+		bottom:0
+		left:0
+		overflow:hidden
+		z-index:1
+		.search-item
+			line-height:.62rem
+			padding-left:.2rem
+			background:#fff
 			color:#666
 </style>
