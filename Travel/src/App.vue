23a2744893926cc86ae7c4e1549db019080a7f43
heroinelux <18810712191@163.com>
2018-06-24 19:31:46 +0800

    finish city

--- Travel/src/App.vue
+++ Travel/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <main>
-      <router-view></router-view>
+    	<keep-alive>
+    		<router-view></router-view>
+    	</keep-alive>   
     </main>
   </div>
 </template>
