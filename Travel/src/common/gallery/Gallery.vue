d76a5cdd657dde3b0cc22204f76ab34cbb1a9134
heroinelux <18810712191@163.com>
2018-06-25 14:19:10 +0800

    add detail banner

---
+++ Travel/src/common/gallery/Gallery.vue
@@ -0,0 +1,64 @@
+<template>
+	<div class="container" @click="handleGalleryClick">
+		<div class="wrapper">
+			<swiper :options="swiperOption" v-if="show">
+	            <swiper-slide>
+	                <img class="gallery-img" src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_800x800_70debc93.jpg">
+	            </swiper-slide>
+	            <swiper-slide>
+	                <img class="gallery-img" src="http://img1.qunarzz.com/sight/p0/1709/76/7691528bc7d7ad3ca3.img.png_800x800_9ef05ee7.png">
+	            </swiper-slide>
+	            <div class="swiper-pagination"  slot="pagination"></div>
+	        </swiper>
+		</div>	
+	</div>
+</template>
+
+<script>
+	export default {
+		name:"CommonGallery",
+		data(){
+			return {
+				show:true,
+				swiperOption:{
+                    pagination:".swiper-pagination",
+                    paginationType:"fraction",
+                   	observer:true,
+    				observeParents:true,
+                }
+			}
+		},
+		methods:{
+			handleGalleryClick(){
+				this.$emit("close");
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+	.container >>> .swiper-container
+		overflow:inherit
+	.container
+		display:flex
+		flex-direction:column
+		justify-content:center
+		position:fixed
+		z-index:10
+		top:0
+		right:0
+		bottom:0
+		left:0
+		background:#000
+		color:#fff
+		.wrapper
+			height:0
+			width:100%
+			padding-bottom:100%
+			.gallery-img
+				width:100%
+			.swiper-pagination
+				color:#fff
+				bottom:-1rem
+				z-index:100
+</style>
---
+++ Travel/src/pages/detail/components/Banner.vue
@@ -0,0 +1,72 @@
+<template>
+	<div>
+		<div class="banner" @click="handleBannerClick">
+			<img class="banner-img" src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg" alt="banner-img">
+			<div class="banner-info">
+				<div class="banner-title">大连圣亚海洋世界(AAAA景区)</div>
+				<div class="banner-number">
+					<span class="iconfont banner-icon">&#xe600;</span>
+					39
+				</div>
+			</div>
+		</div>
+		<common-gallery v-show="showGallery"  @close="handleClose"></common-gallery>
+	</div>
+	
+</template>
+
+<script>
+	import CommonGallery from "@/common/gallery/Gallery"
+	export default{
+		name:"DetailBanner",
+		data(){
+			return {
+				showGallery:false
+			}
+		},
+		components:{
+			CommonGallery,
+		},
+		methods:{
+			handleBannerClick(){
+				this.showGallery=true;
+			},
+			handleClose(){
+				this.showGallery=false;
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+	.banner
+		height:0
+		padding-bottom:55%
+		overflow:hidden
+		position:relative
+		.banner-img
+			width:100%
+		.banner-info
+			position:absolute
+			bottom:0
+			left:0
+			right:0
+			display:flex
+			color:#fff
+			line-height:.6rem
+			background-image:linear-gradient(top,rgba(0,0,0,0),rgba(0,0,0,.8));
+			.banner-title
+				flex:1
+				font-size:.32rem
+				padding:0 .2rem
+			.banner-number
+				padding:0 .4rem
+				height:.32rem
+				line-height:.32rem
+				margin-top:.14rem
+				border-radius:.2rem
+				background:rgba(0,0,0,.8)
+				font-size:.24rem
+				.banner-icon
+					font-size:.24rem
+</style>
