1ece21521115cbb83e45966c028bdd54a78d78bc
heroinelux <18810712191@163.com>
2018-06-25 21:24:07 +0800

    add detail list

--- Travel/src/pages/detail/Detail.vue
+++ Travel/src/pages/detail/Detail.vue
@@ -2,17 +2,41 @@
 	<div class="detail">
 		<detail-banner></detail-banner>
 		<detail-header></detail-header>
+		<detail-list :list="list"></detail-list>
 	</div>
 </template>
 
 <script>
 	import DetailBanner from "./components/Banner.vue"
 	import DetailHeader from "./components/Header.vue"
+	import DetailList from "./components/List.vue"
 	export default{
 		name:"Detail",
 		components:{
 			DetailBanner,
 			DetailHeader,
+			DetailList
+		},
+		data(){
+			return {
+				list:[{
+					title:"成人票",
+					children:[{
+						title:"成人三馆联票",
+						children:[{
+							title:"成人三馆联票-某一连锁店销售"
+						}]
+					},{
+						title:"成人五馆联票"
+					}]
+				},{
+					title:"学生票"
+				},{
+					title:"儿童票"
+				},{
+					title:"特惠票"
+				}]
+			}
 		}
 	}
 </script>
